5d26fff4c257e56a7ff11ab1fa430c271589b065
Eberhard Schneider <mail@eberhardschneider.info>
2019-04-24 13:25:54 +0200

    add composer.json/metadata.php

---
+++ metadata.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This file is part of OXID eShop Logger Demo module.
+ * Logger Demo module is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Logger Demo module is distributed in
+ * the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Logger Demo module.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category      module
+ * @package       loggerdemo
+ * @author        OXID eSales AG
+ * @link          http://www.oxid-esales.com/en/
+ * @copyright (C) OXID e-Sales, 2003-2015
+ */
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.0';
+/**
+ * Module information
+ */
+$aModule = array(
+    'id'          => 'oxiddemomodule',
+    'title'       => [
+        'de' => 'Oxid Demo Module',
+        'en' => 'Oxid Demo Module',
+    ],
+    'description' => [
+        'de' => 'Show price difference to free shipping limit',
+        'en' => 'Show price difference to free shipping limit',
+    ],
+    'version'     => '2.0.0',
+    'author'      => 'Eberhard Schneider',
+    'extend'      => [
+        \OxidEsales\Eshop\Application\Model\Basket::class => \OxidEsales\LoggerDemo\Model\Basket::class,
+    ],
+);
